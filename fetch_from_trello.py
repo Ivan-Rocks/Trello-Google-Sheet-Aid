23ce09eb391e9eeb41a171b15e00dbd1c02bb9f8
Ivan-Rocks <13611041610@163.com>
2022-01-11 01:47:18 -0600

    date and notice finished

--- fetch_from_trello.py
+++ fetch_from_trello.py
@@ -2,23 +2,44 @@ from requests_oauthlib import OAuth1Session
 from pprint import pp, pprint
 import json
 
+from commands import get_remaining_sessions
 
-def convert_data(data):
+
+# Get the list of existing boards
+def get_boards(data):
+    api_key = data['key']
+    api_secret = data['OAuth']
+    token = data['token']
+    trello = OAuth1Session(api_key, api_secret, token)
+    url = 'https://api.trello.com/1/members/me/boards?key=add7e1644b237dd2dae95b7b2f39c3b4' \
+          '&token=0732d1e3c9b5528454803f7d4992b1579ad4f821ab98ba61ec3c1afc1e7da690'
+    r = trello.get(url)
+    with open("JSON/Student List.json", 'w') as outfile:
+        json.dump(r.json(), outfile)
+    return r.json()
+
+
+# Fetch board information from Trello and convert to JSON
+def convert_data(data, active_student_list):
+    count = 0
     for i in data:
-        board_name = i[0]
-        api_key = i[3]
-        api_secret = i[4]
-        token = i[1]
-        short_id = i[5]
+        board_name = i['name']
+        if board_name not in active_student_list:
+            continue
+        api_key = 'add7e1644b237dd2dae95b7b2f39c3b4'
+        api_secret = '89fb5caca76cc0c24bdd7e66222b33aaaf1fad668cb6f890c397ad302bb905f1'
+        token = '0732d1e3c9b5528454803f7d4992b1579ad4f821ab98ba61ec3c1afc1e7da690'
+        short_id = i['shortLink']
         trello = OAuth1Session(api_key, api_secret, token)
         r = trello.get(generate_board_request_url(short_id, board_name))
-        pprint(r.json())
-        print(r.status_code)
-        print(board_name)
         content = r.json()
         write_to_json(board_name, content)
+        count += 1
+        print("List Converted " + str(count) + "/" + str(len(active_student_list)))
+    print('Conversion Completed')
 
 
+# Helper function to generate url request to access boards
 def generate_board_request_url(short_id, board_name):
     url = 'https://trello.com/b/'
     url = url + short_id + '/'
@@ -26,9 +47,16 @@ def generate_board_request_url(short_id, board_name):
     return url
 
 
+# Write boards into JSON file
 def write_to_json(board_name, content):
     directory = 'JSON/' + board_name + '.json'
     with open(directory, 'w') as outfile:
         json.dump(content, outfile)
 
 
+# Given a board name, return the JSON board file of that user
+def get_user_data(board_name):
+    directory = 'JSON/' + board_name + '.json'
+    with open(directory) as json_file:
+        user_json = json.load(json_file)
+    return user_json
--- main.py
+++ main.py
@@ -6,11 +6,34 @@ import numpy as np
 from oauth2client.service_account import ServiceAccountCredentials
 import gspread
 import json
+import xlsxwriter
+from datetime import datetime
 
 # From Own Script
-import commands
 import fetch_from_trello
 import commands
+
+# 建立Local Sheet
+student_workbook = xlsxwriter.Workbook('Results/Student Info.xlsx')
+students = student_workbook.add_worksheet()
+notice_workbook = xlsxwriter.Workbook('Results/Notice.xlsx')
+notice = notice_workbook.add_worksheet()
+
+# Extract local student list
+active_file = open("Active Student List.txt", "r", encoding='utf-8', errors='ignore')
+active_student_list = active_file.read().splitlines()
+
+# 绘制Student Info表头
+students.write(0, 0, 'Name')
+students.write(0, 1, '总课时')
+students.write(0, 2, '剩余课时')
+students.write(0, 3, 'Current Class')
+students.write(0, 4, '多少天没上课')
+
+# 绘制Notice表头
+notice.write(0, 0, 'Notice')
+notice.write(0, 1, 'Students')
+notice.write(1, 0, '三周没上课')
 
 # Authorize with Google
 scope = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file",
@@ -19,44 +42,49 @@ credential = ServiceAccountCredentials.from_json_keyfile_name('Auth Info/Google 
 client = gspread.authorize(credential)
 sheet = client.open("Demo Sheet").worksheet("Test")
 
-# 绘制Google Sheet表头
-sheet.update_cell(1, 1, 'Name')
-sheet.update_cell(1, 2, '课时')
-
 # Extract Data from Trello
 with open('Auth Info/Trello Auth.json') as json_file:
     auth_data = json.load(json_file)
 board_list = fetch_from_trello.get_boards(auth_data)  # get list of boards
-fetch_from_trello.convert_data(board_list)
-
-sheet.update_cell(1, 1, 'Name')
-sheet.update_cell(1, 2, '课时')
-sheet.update_cell(1, 3, 'Time')
-
-board_name = board_list[26]['name']
-sheet.update_cell(2, 1, board_name)
-data = fetch_from_trello.get_user_data(board_name)
-sessions = commands.get_sessions(data['cards'], data['lists'])
-sheet.update_cell(2, 2, sessions)
-time = commands.get_time(data['cards'])
-print(time)
-
-print('Finished updating to Google Sheet')
+fetch_from_trello.convert_data(board_list, active_student_list)
 
 # Run Commands and Update to Google Sheet
-count = 1
+count = 0
+absent_list = []
+
 for board in board_list:
-    count += 1
+    # Identify whether to track this board
     board_name = board['name']
-    sheet.update_cell(count, 1, board_name)
-    data = fetch_from_trello.get_user_data(board_name)
-    sessions = commands.get_sessions(data['cards'], data['lists'])
-    sheet.update_cell(count, 2, sessions)
-    class_time = commands.get_time(data['cards'])
-    for i in range(11):
-        sheet.update_cell(count, 3 + i, class_time[i])
-    if count % 30 == 0:
-        sleep(100)
-        print('Sleeping Started')
-    if count % 30 == 0:
-        print('Sleeping Finished')
+    if board_name not in active_student_list:
+        continue
+
+    count += 1  # Set Counter
+
+    # Retrieve data from trello
+    students.write(count, 0, board_name)
+    data = fetch_from_trello.get_user_data(board_name)  # Fetch JSON file
+
+    # Basic variables
+    TODO_id = commands.get_TODO_id(data['lists'])
+    Doing_id = commands.get_Doing_id(data['lists'])
+    Done_id = commands.get_Done_id(data['lists'])
+
+    # Run them commands
+    total_sessions = commands.get_total_sessions(data['cards'])
+    students.write(count, 1, total_sessions)
+    remaining_sessions = commands.get_remaining_sessions(data['cards'], TODO_id, total_sessions)
+    students.write(count, 2, remaining_sessions)
+    current_session = commands.get_current_class(data['cards'], Doing_id, Done_id)
+    students.write(count, 3, current_session)
+    absence_time = commands.get_absence_time(data['cards'], current_session)
+    students.write(count, 3, absence_time)
+    if absence_time != 'NA' and int(absence_time) > 21:
+        absent_list.append(board_name)
+
+print(absent_list)
+for i in range(0, len(absent_list)):
+    notice.write(1, i + 1, absent_list[i])
+
+# Close local sheet and push to Google
+student_workbook.close()
+notice_workbook.close()
